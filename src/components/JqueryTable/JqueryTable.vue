87c042ef9605ec42038aafed9b7417511e87efbf
Stas Lagoda <lagoda.stasik@gmail.com>
2023-08-22 15:51:27 +0300

    first commit

---
+++ src/components/JqueryTable/JqueryTable.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h2>Implement jQuery DataTable in Vue Js</h2>
+    <table class="table" id="datatable">
+      <thead>
+      <tr>
+        <th>ID</th>
+        <th>Product Title</th>
+        <th>Product Price</th>
+        <th>Created On</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="item in products" :key="item.DT_RowId">
+        <td>{{ item.DT_RowId }}</td>
+        <td>{{ item.colors.id }}</td>
+        <td>{{ item.colors.name }}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script>
+import "jquery/dist/jquery.min.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "datatables.net-dt/js/dataTables.dataTables";
+import "datatables.net-dt/css/jquery.dataTables.min.css";
+import axios from "axios";
+import $ from "jquery";
+export default {
+  mounted() {
+    axios.get("/colors.json").then((response) => {
+      this.products = response.data.data;
+      // console.log('this.products');
+      // console.log(this.products);
+      $("#datatable").DataTable();
+    });
+  },
+  data: function () {
+    return {
+      products: [],
+    };
+  },
+};
+</script>
---
+++ src/views/app/datatables/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div className="container mt-4">
+    <JqueryTable/>
+  </div>
+</template>
+<script>
+import JqueryTable from '../../../components/JqueryTable/JqueryTable.vue'
+
+export default {
+  name: 'App',
+  components: {
+    JqueryTable
+  }
+}
+</script>
+<style>
+.container {
+  max-width: 1000px;
+}
+</style>
